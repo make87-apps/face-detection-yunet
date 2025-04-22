74f35eb73ec3b6c271a94224681b507dd46de3fd
todo@make87.com <make87>
2025-04-22 22:15:51 +0000

    outputting bounding boxes now

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import make87
 from make87_messages.geometry.box.box_2d_aligned_pb2 import Box2DAxisAligned
+from make87_messages.geometry.box.boxes_2d_aligned_pb2 import Boxes2DAxisAligned
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
 from make87_messages.core.header_pb2 import Header
 
@@ -12,13 +13,19 @@ from make87_messages.core.header_pb2 import Header
 def main():
     make87.initialize()
     image_topic = make87.get_subscriber(name="IMAGE_DATA", message_type=ImageJPEG)
-    bbox_2d_topic = make87.get_publisher(name="BOUNDING_BOX_2D", message_type=Box2DAxisAligned)
+    bbox_2d_topic = make87.get_publisher(
+        name="BOUNDING_BOX_2D", message_type=Boxes2DAxisAligned
+    )
 
     model_path = files("app") / "res" / "face_detection_yunet_2023mar.onnx"
-    face_detector = cv2.FaceDetectorYN.create(model=str(model_path), config="", input_size=(0, 0))
+    face_detector = cv2.FaceDetectorYN.create(
+        model=str(model_path), config="", input_size=(0, 0)
+    )
 
     def callback(message: ImageJPEG):
-        image = cv2.imdecode(np.frombuffer(message.data, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        image = cv2.imdecode(
+            np.frombuffer(message.data, dtype=np.uint8), cv2.IMREAD_UNCHANGED
+        )
         logging.info(f"Received image with shape: {image.shape}")
 
         height, width, _ = image.shape
@@ -26,14 +33,31 @@ def main():
 
         _, faces = face_detector.detect(image)
 
-        faces = faces if faces is not None else []
-        for face in faces:
-            header = make87.header_from_message(
-                header_cls=Header, message=message, append_entity_path="face", set_current_time=True
-            )
-            bbox_2d = Box2DAxisAligned(x=face[0], y=face[1], width=face[2], height=face[3], header=header)
-            bbox_2d_topic.publish(bbox_2d)
-            logging.info(f"Published bounding box: {bbox_2d}")
+        header = make87.header_from_message(
+            header_cls=Header,
+            message=message,
+            append_entity_path="faces",
+            set_current_time=True,
+        )
+
+        if faces is not None:
+            bboxes_2d = Boxes2DAxisAligned(header=header, boxes=[])
+            for i, face in enumerate(faces):
+                bbox_2d = Box2DAxisAligned(
+                    x=face[0],
+                    y=face[1],
+                    width=face[2],
+                    height=face[3],
+                    header=make87.header_from_message(
+                        header_cls=Header,
+                        message=bboxes_2d,
+                        append_entity_path=f"{i}",
+                        set_current_time=False,
+                    ),
+                )
+                bboxes_2d.boxes.append(bbox_2d)
+            bbox_2d_topic.publish(bboxes_2d)
+            logging.info(f"Published bounding boxes: {bboxes_2d}")
 
     image_topic.subscribe(callback)
     make87.loop()
